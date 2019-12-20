18e3bccc613d22a9f6401fbf7ff05bac21a96f24
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-19 20:13:47 -0500

    alert the user to enter the bill amount

--- assets/js/TipCalculator.js
+++ assets/js/TipCalculator.js
@@ -1,21 +1,29 @@
-$(".btn").on(click, function() {
-  var billType = $("#bill-type")
-    .val()
-    .trim();
-  var totalAmount = $("bill-amount")
-    .val()
-    .trim();
-  var tipPercentage = $("bill-tip")
-    .val()
-    .trim();
-  var totalPeople = $("bill-sharing")
-    .val()
-    .trim();
+$(document).ready(function() {
+  $(".btn").click(function() {
+    var billType = $("#bill-type")
+      .val()
+      .trim();
+    // console.log(billType);
+    var totalAmount = $("#bill-amount")
+      .val()
+      .trim();
+    // console.log(totalAmount);
+    var tipPercentage = $("#bill-tip")
+      .val()
+      .trim();
+    var totalPeople = $("#bill-sharing")
+      .val()
+      .trim();
 
-  //Quick validation
-  if (totalAmount === "") {
-    alert("Please enter the amount");
-    return;
-  }
-  //Check to see if this input is empty or less than or equal to 1
+    //Quick validation
+    if (totalAmount === "") {
+      alert("Please enter the bill amount");
+      return;
+    }
+    //Check to see if this input is empty or less than or equal to 1
+    if (totalPeople === "" || totalPeople <= 1) {
+      totalPeople = 1;
+      document.getElementById("each").style.display = "none";
+    }
+  });
 });
