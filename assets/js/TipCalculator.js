e315da37ce7655df3868d1d1c134f2157d1cf376
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-19 22:48:48 -0500

    tip is calculated for each person

--- assets/js/TipCalculator.js
+++ assets/js/TipCalculator.js
@@ -1,29 +1,40 @@
 $(document).ready(function() {
   $(".btn").click(function() {
-    var billType = $("#bill-type")
-      .val()
-      .trim();
     // console.log(billType);
-    var totalAmount = $("#bill-amount")
-      .val()
-      .trim();
-    // console.log(totalAmount);
-    var tipPercentage = $("#bill-tip")
-      .val()
-      .trim();
-    var totalPeople = $("#bill-sharing")
-      .val()
-      .trim();
+    var amount = parseFloat(
+      $("#bill-amount")
+        .val()
+        .trim()
+    );
+    // console.log(amount);
+    var tipPercentage = parseFloat(
+      $("#bill-tip")
+        .val()
+        .trim()
+    );
+    var totalPeople = parseInt(
+      $("#bill-sharing")
+        .val()
+        .trim()
+    );
 
     //Quick validation
-    if (totalAmount === "") {
+    if (amount <= 0 || tipPercentage <= 0) {
       alert("Please enter the bill amount");
       return;
     }
     //Check to see if this input is empty or less than or equal to 1
     if (totalPeople === "" || totalPeople <= 1) {
       totalPeople = 1;
-      document.getElementById("each").style.display = "none";
+      $("#each").hide();
+    } else {
+      $("#each").show();
     }
+    var tips = amount * (tipPercentage / 100);
+    var totalAmount = tips + amount;
+    var amountPerPerson = totalAmount / totalPeople;
+
+    //Display the tip!
+    $("#tip").text(amountPerPerson);
   });
 });
